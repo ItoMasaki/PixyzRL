2159c28a325358f1a07dc8712cfb424ecc730752
ItoMasaki <ito-ma@wni.com>
2025-03-27 20:44:28 +0900

    Update CarRacing PPO example; adjust environment parameters, modify neural network activation functions, and enhance buffer size

--- examples/car_racing_v3_ppo_continual.py
+++ examples/car_racing_v3_ppo_continual.py
@@ -9,7 +9,7 @@ from pixyzrl.models import PPO
 from pixyzrl.trainer import OnPolicyTrainer
 from pixyzrl.utils import print_latex
 
-env = Env("CarRacing-v3", 8, render_mode="rgb_array")
+env = Env("CarRacing-v3", 2, render_mode="rgb_array")
 action_dim = env.action_space
 obs_dim = env.observation_space[::-1]
 
@@ -20,16 +20,18 @@ class Actor(Normal):
 
         self.feature_extract = nn.Sequential(
             nn.LazyConv2d(32, kernel_size=8, stride=4),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.LazyConv2d(64, kernel_size=4, stride=2),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.LazyConv2d(64, kernel_size=3, stride=1),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.Flatten(),
         )
 
         self._loc = nn.Sequential(
             nn.LazyLinear(1024),
+            nn.Tanh(),
+            nn.LazyLinear(128),
             nn.Tanh(),
             nn.LazyLinear(action_dim),
             nn.Tanh(),
@@ -37,6 +39,8 @@ class Actor(Normal):
 
         self._scale = nn.Sequential(
             nn.LazyLinear(2048),
+            nn.Tanh(),
+            nn.LazyLinear(128),
             nn.Tanh(),
             nn.LazyLinear(action_dim),
             nn.Softplus(),
@@ -55,16 +59,18 @@ class Critic(Deterministic):
 
         self.feature_extract = nn.Sequential(
             nn.LazyConv2d(32, kernel_size=8, stride=4),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.LazyConv2d(64, kernel_size=4, stride=2),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.LazyConv2d(64, kernel_size=3, stride=1),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.Flatten(),
         )
 
         self.net = nn.Sequential(
             nn.LazyLinear(1024),
+            nn.Tanh(),
+            nn.LazyLinear(128),
             nn.Tanh(),
             nn.LazyLinear(1),
         )
@@ -82,32 +88,49 @@ ppo = PPO(
     actor,
     critic,
     entropy_coef=0.01,
-    mse_coef=0.5,
+    mse_coef=1.0,
     lr_actor=1e-4,
     lr_critic=3e-4,
     device="mps",
+    clip_grad_norm=0.5,
 )
 print_latex(ppo)
 
 buffer = RolloutBuffer(
-    5120,
+    2048,
     {
         "obs": {
             "shape": (*obs_dim,),
             "map": "o",
+            "transform": lambda x: x / 255.0,
         },
-        "value": {"shape": (1,), "map": "v"},
-        "action": {"shape": (action_dim,), "map": "a"},
-        "reward": {"shape": (1,)},
-        "done": {"shape": (1,)},
-        "returns": {"shape": (1,), "map": "r"},
-        "advantages": {"shape": (1,), "map": "A"},
+        "value": {
+            "shape": (1,),
+            "map": "v",
+        },
+        "action": {
+            "shape": (action_dim,),
+            "map": "a",
+        },
+        "reward": {
+            "shape": (1,),
+        },
+        "done": {
+            "shape": (1,),
+        },
+        "returns": {
+            "shape": (1,),
+            "map": "r",
+        },
+        "advantages": {
+            "shape": (1,),
+            "map": "A",
+        },
     },
-    8,
+    2,
     advantage_normalization=True,
 )
 
 logger = Logger("car_racing_v3_ppo_continual", log_types=["print"])
 trainer = OnPolicyTrainer(env, buffer, ppo, "gae", "mps", logger=logger)
-# trainer.load_model("cartpole_v1_ppo_discrete_trainer/model_1200.pt")
 trainer.train(1000000, 512, 10, save_interval=50, test_interval=20)
